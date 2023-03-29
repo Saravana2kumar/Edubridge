fcddbba7d0c5dd587e460e387fdb8d59e6f91004
Saravana2kumar <129151625+Saravana2kumar@users.noreply.github.com>
2023-03-29 09:55:20 +0530

    Add files via upload

---
+++ Family.java
@@ -0,0 +1,13 @@
+public class Family
+{
+	public static void main(String args[])
+	{
+		String father="Aiyeram",mother="Mariyammal",city="Mumbai",state="Maharashtra",country="India";
+		int age=22,salary=15000;
+		System.out.println("Father Name:"+father);
+		System.out.println("Mother Name:"+mother);
+		System.out.println("Age:"+age);
+		System.out.println("Salary:"+salary);
+		System.out.println("Address:"+city+" " +state+" " +country);
+	}
+}
